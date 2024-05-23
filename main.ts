1b89100be937d01a9be87e6fb004dfb0f9da43b7
Peter Larson <peterjohnlarson@gmail.com>
2024-05-22 23:56:04 -0600

    Axiom 61 Adaptation

--- main.ts
+++ main.ts
@@ -4,45 +4,49 @@ import { createMotuClient } from "./motu-client";
 
 const client = createMotuClient({
   labels: {
-    'mix/chan/0/matrix/fader': 'MIC',
-    'mix/chan/1/matrix/fader': 'INST',
-    'mix/chan/2/matrix/fader': 'CAST',
-    'mix/chan/4/matrix/fader': 'AUX',
-    'mix/chan/8/matrix/fader': 'OS',
-    'mix/chan/10/matrix/fader': 'DAW',
-    'mix/monitor/0/matrix/fader': 'PHONES',
+    'mix/chan/1/matrix/fader': 'MIC',
+    'mix/chan/2/matrix/fader': 'INST',
+    'mix/chan/4/matrix/fader': 'CAST',
+    'mix/chan/10/matrix/fader': 'TV',
+    'mix/chan/12/matrix/fader': 'WORK',
+    'mix/chan/18/matrix/fader': 'OS',
+    'mix/chan/20/matrix/fader': 'DAW',
+    'mix/monitor/1/matrix/fader': 'PHONES',
     'mix/main/0/matrix/fader': 'MAIN',
   }
 });
 
 console.log('MIDI inputs:', midi.getInputs())
-const MIDI_INPUT = 'MPK261 Port A';
-const FADER_CC_ARRAY = [18, 21, 22, 23, 24, 25, 26, 27]
+const MIDI_INPUT = 'Axiom 61 Axiom USB In';
+const FADER_CC_ARRAY = [16, 17, 18, 19, 3, 61, 47, 107, 7]
 
 const input = new midi.Input(MIDI_INPUT);
 input.on('cc', (msg) => {
   if (msg.controller === FADER_CC_ARRAY[0]) {
     // MIC
-    client.set('mix/chan/0/matrix/fader', Math.pow(msg.value / 127, 4));
+    client.set('mix/chan/1/matrix/fader', Math.pow(msg.value / 127, 4));
   } else if (msg.controller === FADER_CC_ARRAY[1]) {
     // INST
-    client.set('mix/chan/1/matrix/fader', Math.pow(msg.value / 127, 4));
+    client.set('mix/chan/2/matrix/fader', Math.pow(msg.value / 127, 4));
   } else if (msg.controller === FADER_CC_ARRAY[2]) {
     // CAST
-    client.set('mix/chan/2/matrix/fader', Math.pow(msg.value / 127, 4));
-  } else if (msg.controller === FADER_CC_ARRAY[3]) {
-    // AUX
     client.set('mix/chan/4/matrix/fader', Math.pow(msg.value / 127, 4));
-  } else if (msg.controller === FADER_CC_ARRAY[4]) {
-    // OS
-    client.set('mix/chan/8/matrix/fader', Math.pow(msg.value / 127, 4));
-  } else if (msg.controller === FADER_CC_ARRAY[5]) {
-    // DAW
+  } else if (msg.controller === FADER_CC_ARRAY[3]) {
+    // TV
     client.set('mix/chan/10/matrix/fader', Math.pow(msg.value / 127, 4));
+  } else if (msg.controller === FADER_CC_ARRAY[4]) {
+    // WORK
+    client.set('mix/chan/12/matrix/fader', Math.pow(msg.value / 127, 4));
+  } else if (msg.controller === FADER_CC_ARRAY[5]) {
+    // OS
+    client.set('mix/chan/18/matrix/fader', Math.pow(msg.value / 127, 4));
   } else if (msg.controller === FADER_CC_ARRAY[6]) {
+    // DAW
+    client.set('mix/chan/20/matrix/fader', Math.pow(msg.value / 127, 4));
+  } else if (msg.controller === FADER_CC_ARRAY[7]) {
     // PHONES
     client.set('mix/monitor/0/matrix/fader', Math.pow(msg.value / 127, 4));
-  } else if (msg.controller === FADER_CC_ARRAY[7]) {
+  } else if (msg.controller === FADER_CC_ARRAY[8]) {
     // MAIN
     client.set('mix/main/0/matrix/fader', Math.pow(msg.value / 127, 4));
   }
